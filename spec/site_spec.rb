ab5972b61b6dff896f5e393c898a709b65bc877c
Bo Turnbow <bo@turnbows.org>
2016-04-06 11:47:47 -0600

    Use html-proofer from rspec

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -1,22 +1,22 @@
-describe 'Jekyll site' do
-  describe 'valid html and internal links' do
-    it 'returns the sum of its arguments' do
-      expect(2).to eq(3)
-    end
-  end
-end
+require 'html-proofer'
+
+# describe 'Jekyll site' do
+#   describe 'valid html and internal links' do
+#     it 'returns the sum of its arguments' do
+#       expect(2).to eq(3)
+#     end
+#   end
+# end
 
 
 describe 'Jekyll site' do
   it 'outputs valid HTML' do
     # site.process
     options = {
-      check_html: true,
+      check_html: true, :validation => { :report_invalid_tags => true, :report_script_embeds => true },
       checks_to_ignore: %w(ScriptCheck LinkCheck ImageCheck)
     }
-    status = HTML::Proofer.new(dest_dir, options).run
+    status = HTMLProofer.check_directory("_site/", options).run
     expect(status).to eql(true)
   end
 end
-
-# just get rspec to do something!!!! that fails and see how that looks locally and in circle :)
