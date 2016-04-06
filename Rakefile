eef01fdaa54c909c82e758b938c6a7ac2ba4f518
Bo Turnbow <bo@turnbows.org>
2016-04-06 16:15:29 -0600

    Really really get rspec tests working

--- Rakefile
+++ Rakefile
@@ -1,7 +1,4 @@
-require 'html-proofer'
 require 'rspec/core/rake_task'
-
-RSpec::Core::RakeTask.new(:spec)
 
 task :default => [:serve]
 
@@ -16,43 +13,6 @@ task :serve do
 end
 
 desc "Test site."
-task :test => ["test:internal", "test:external"]
-
-namespace :test do
-
-  options = {
-    :assume_extension => true,
-    :file_ignore => [
-      "./_site/Christmas_Gift_Show.html",
-      "./_site/board_members.html",
-      "./_site/christmas_craft_show.html",
-      "./_site/fashion_show.html",
-      "./_site/feb_fest.html",
-      "./_site/home.html",
-      "./_site/info.html"
-    ]
-  }
-
-  desc "Test site and internal linking."
-  task :internal => [:build] do
-    HTMLProofer.check_directory(
-      "./_site",
-      {
-        :disable_external => true,
-        :check_favicon => true,
-        :check_html => true
-      }.merge(options)
-    ).run
-  end
-
-  desc "Test external links."
-  task :external => [:build] do
-    HTMLProofer.check_directory(
-      "./_site",
-      {
-        :external_only => true,
-        :cache => { :timeframe => '1h' }
-      }.merge(options)
-    ).run
-  end
+task :test => [:build] do
+  RSpec::Core::RakeTask.new(:test)
 end
--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -1,22 +1,51 @@
 require 'html-proofer'
 
-# describe 'Jekyll site' do
-#   describe 'valid html and internal links' do
-#     it 'returns the sum of its arguments' do
-#       expect(2).to eq(3)
-#     end
-#   end
-# end
+describe 'Web site' do
 
+  options = {
+    :assume_extension => true,
+    :file_ignore => [
+      "_site/Christmas_Gift_Show.html",
+      "_site/board_members.html",
+      "_site/christmas_craft_show.html",
+      "_site/fashion_show.html",
+      "_site/feb_fest.html",
+      "_site/home.html",
+      "_site/info.html"
+    ]
+  }
 
-describe 'Jekyll site' do
   it 'outputs valid HTML' do
-    # site.process
-    options = {
-      check_html: true, :validation => { :report_invalid_tags => true, :report_script_embeds => true },
-      checks_to_ignore: %w(ScriptCheck LinkCheck ImageCheck)
-    }
-    status = HTMLProofer.check_directory("_site/", options).run
+    status = HTMLProofer.check_directory(
+      "_site",
+      {
+        :check_html => true,
+        :checks_to_ignore => %w(ScriptCheck LinkCheck ImageCheck)
+      }.merge(options)
+    ).run
     expect(status).to eql(true)
   end
+
+  it 'is a well-formed site' do
+    status = HTMLProofer.check_directory(
+      "_site",
+      {
+        :disable_external => true,
+        :check_favicon => true
+      }.merge(options)
+    ).run
+    expect(status).to eql(true)
+  end
+
+  it 'has valid external links' do
+    status = HTMLProofer.check_directory(
+      "_site",
+      {
+        :external_only => true,
+        :cache => { :timeframe => '1h' }
+      }.merge(options)
+    ).run
+    expect(status).to eql(true)
+  end
+
 end
